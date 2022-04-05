3847f17bfbfb032a05b23d3117cc3e546e9a27ed
bobo <79424554+Anjfdgbkjfdb@users.noreply.github.com>
2022-04-05 14:05:13 +1200

    added next button that generates new question

--- TimesTable.py
+++ TimesTable.py
@@ -1,22 +1,40 @@
 """Times Table Task"""
 
 from tkinter import *
+import random
+
 
 class TimesTable:
     def __init__(self, parent):
 
-        Label(parent,text="4*5 = ").grid()
+        self.rand1=random.randint(1, 10)
+        self.rand2=random.randint(1, 10)
+        self.ans=self.rand1 * self.rand2
+
         canvas1 = Canvas(parent)
         canvas1.grid()
         self.entry1 = Entry (parent) 
-        canvas1.create_window(300,0,window=self.entry1)
+        canvas1.create_window(300,50,window=self.entry1)
         checkbtn=Button(parent,text="check", command=self.check)
         checkbtn.grid()
+        next=Button(parent,text="next",command=self.next)
+        next.grid()
         self.text=Label(parent, text = "")
         self.text.grid()
+        self.question=Label(parent,text= self.rand1)
+        self.question.grid()
+        self.question1=Label(parent,text= self.rand2)
+        self.question1.grid()
+
+    def next(self):
+        self.rand1=random.randint(1, 10)
+        self.rand2=random.randint(1, 10)
+        self.ans=self.rand1 * self.rand2
+        self.question.configure(text=self.rand1)
+        self.question1.configure(text=self.rand2)
 
     def check(self):
-        if self.entry1.get() == "20":
+        if int(self.entry1.get()) == self.ans :
             self.text.configure(text="Correct")
             self.text.grid()
         else:
